cd8c350d5f5648dd2fe8bf5d3e77a51cf0b54c00
Jeferson Goncalves <jeferson.goncalves.noronha@gmail.com>
2021-10-22 18:05:51 -0300

    interface dashbord completa

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,69 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
 
+import logoImg from '../../assets/logoGitExplore.svg';
+import perfilImg from '../../img/53b3dd1b2557a8cab4b8816544fcfc39.jpg';
 // FC = FunctionComponent
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github explore" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositorio" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src={perfilImg} alt="foto de perfil" />
+
+          <div>
+            <strong> nome do repositorio </strong>
+            <p> breve descrição do repositorio </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img src={perfilImg} alt="foto de perfil" />
+
+          <div>
+            <strong> nome do repositorio </strong>
+            <p> breve descrição do repositorio </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img src={perfilImg} alt="foto de perfil" />
+
+          <div>
+            <strong> nome do repositorio </strong>
+            <p> breve descrição do repositorio </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img src={perfilImg} alt="foto de perfil" />
+
+          <div>
+            <strong> nome do repositorio </strong>
+            <p> breve descrição do repositorio </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
