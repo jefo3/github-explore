7d21b789b80a886d3b5a351171940ff778e662a9
Jeferson Goncalves <jeferson.goncalves.noronha@gmail.com>
2021-11-01 00:47:30 -0300

    estilizando a pagina de detalhe do repositorio

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issue } from './styles';
+
+import logoImg from '../../assets/logoGitExplore.svg';
+import imge from '../../img/53b3dd1b2557a8cab4b8816544fcfc39.jpg';
 
 interface RepositoryParams {
   repository: string;
@@ -7,7 +13,55 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHubExplore" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src={imge} alt="fotoPerfil" />
+          <div>
+            <strong> Titulo repo </strong>
+            <p> descrinção </p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>10990 </strong>
+            <span> Stars </span>
+          </li>
+
+          <li>
+            <strong>10990</strong>
+            <span> Forks </span>
+          </li>
+
+          <li>
+            <strong>10990</strong>
+            <span> Issues abertas </span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issue>
+        <Link to="adssada">
+          <div>
+            <strong> repository.full_name </strong>
+            <p> repository.description </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issue>
+    </>
+  );
 };
 
 export default Repository;
